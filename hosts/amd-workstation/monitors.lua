408aa36514cfd276f919a4838e039f242765cc72
Cody <codyjohnson144@gmail.com>
2026-08-02 13:57:46 -0500

    amd-workstation monitors

--- hosts/amd-workstation/monitors.lua
+++ hosts/amd-workstation/monitors.lua
@@ -2,7 +2,7 @@
 -- Replaces monitor = ... from hyprland-monitors.conf
 
 hl.monitor({
-    output = "DP-3",
+    output = "DP-1",
     mode = "2560x1440@144",
     position = "0x0",
     scale = 1,
@@ -12,5 +12,12 @@ hl.monitor({
     output = "DP-2",
     mode = "1920x1080@60",
     position = "2560x0",
+    scale = 1,
+})
+
+hl.monitor({
+    output = "DP-3",
+    mode = "1920x1080@60",
+    position = "4480x0",
     scale = 1,
 })
